d2f3d23437f3648ca89d7b20eeba9b15707bb937
Hyunggi <phg9587@naver.com>
2022-05-18 14:37:07 +0900

    기본 파일 업로드

---
+++ src/main/java/com/test/ci_cd_test/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.test.ci_cd_test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String test(){
+        return "Hello, Test #1";
+    }
+}
