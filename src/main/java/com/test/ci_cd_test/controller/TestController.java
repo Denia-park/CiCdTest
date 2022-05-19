df4671b8440b385a856ee3668c6415df37fb9ecd
Hyunggi <phg9587@naver.com>
2022-05-20 00:09:13 +0900

    Test를 위해서 Controller 수정

--- src/main/java/com/test/ci_cd_test/controller/TestController.java
+++ src/main/java/com/test/ci_cd_test/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.test.ci_cd_test.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.url}")
+    private String OPENVIDU_URL;
+
     @GetMapping("/")
     public String test(){
-        return "Hello, Test -- Finish";
+        return "Hello, Test -- Finish  " + OPENVIDU_URL;
     }
 }
