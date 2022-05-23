395af695f9563d2533a41cd93dd03354cae5dada
Hyunggi <phg9587@naver.com>
2022-05-23 23:49:36 +0900

    EC2에서 AWS 동작안하는 이슈 확인중

--- src/main/java/com/test/ci_cd_test/controller/TestController.java
+++ src/main/java/com/test/ci_cd_test/controller/TestController.java
@@ -1,17 +1,25 @@
 package com.test.ci_cd_test.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
-
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
     @Value("${test.url}")
     private String OPENVIDU_URL;
 
+    private final AmazonS3 amazonS3;
+
     @GetMapping("/")
     public String test(){
-        return "Hello, Test -- Finish  " + OPENVIDU_URL;
+        String imgUrl = amazonS3.getUrl(bucket, "123456").toString();
+        return "Hello, Test -- Finish  " + OPENVIDU_URL + "      \n"
+                + imgUrl;
     }
 }
