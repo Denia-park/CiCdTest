93704a010459223f8840a40abd63e3508d448231
Hyunggi <phg9587@naver.com>
2022-05-24 00:24:04 +0900

    테스트 파일을 제외하고 다시 테스트

--- src/test/java/com/test/ci_cd_test/CiCdTestApplicationTests.java
+++ src/test/java/com/test/ci_cd_test/CiCdTestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CiCdTestApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
